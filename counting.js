63f0e618b00ebe49343c11f3fd74ec4f107742c5
Mohammad Zubair <mohammadzubair@Mohammads-MacBook-Air.local>
2020-06-05 17:50:52 +0530

    changes

---
+++ counting.js
@@ -0,0 +1,19 @@
+// for(var i = 1; i<= 100; i= i+3){
+//     var modulo = i % 2;
+//     if(modulo === 0){
+//         console.log(i, "---even")
+//     }else{
+//         console.log(i, "---odd")
+//     }
+// }           
+for(var i = 1; i<= 10; i++){
+    var result= i*3;
+    var modulo= result % 2;
+    if(modulo === 0){
+        console.log(result, "--even")
+    }else{
+        console.log(result, "--odd")
+
+    }
+
+}                                                                              
---
+++ pahada.js
@@ -0,0 +1,12 @@
+// var pahada = 10
+// for(var i= 1; i<= 10; i = i+2){
+//     var result =pahada*i;
+//     console.log(result);
+
+// }
+// for(var i= 1; i<= 100; i = i+3){
+//      console.log(i);
+// }
+
+
+
